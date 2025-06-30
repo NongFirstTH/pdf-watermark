628600966f3d299f5646e4f2b37b196e256a17fa
NongFirstTH <TanabodeeChannel@gmail.com>
2025-06-30 22:32:52 +0700

    fix unuse import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 import "./globals.css";
-import Navbar from "@/components/header";
-import Footer from "@/components/footer";
 import ColorfulWebsite from "./colorful";
 
 const geistSans = Geist({
